0dfa7c4bffc95489af0c6e32315ab28586f41cc6
Tommaso Perondi <tommaso2001.pero@gmail.com>
2018-05-09 23:31:29 +0200

    added account verification

--- js/login.js
+++ js/login.js
@@ -1,3 +1,10 @@
+function Get(yourUrl){
+    var Httpreq = new XMLHttpRequest(); // a new request
+    Httpreq.open("GET",yourUrl,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
 $(function () {
     $('form').on('submit',function (e) {
 
@@ -6,6 +13,15 @@ $(function () {
         var usr_re = /[a-zA-Z0-9]/;
         if(pwd_re.test(form[1].value) && usr_re.test(form[0].value)){
             console.log("Debug: Form OK");
+            var users_obj = JSON.parse(Get("https://raw.githubusercontent.com/tommaso-perondi/eshop/master/login.json"));
+            var found=0;
+            for (var user in users_obj.users) {
+                if(users_obj.users[user].name==form[0].value && users_obj.users[user].password==form[1].value) var found=1;
+            }
+            if(found==1)window.location.replace("shop.html");
+            else {
+                //da mettere label di errore
+            }
         }else console.log("Debug: Form ERROR");
           e.preventDefault();
         });
