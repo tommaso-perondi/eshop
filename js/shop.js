dd5640057a0d83947ce5183266450c3a98db5caa
Tommaso Perondi <38632192+tommaso-perondi@users.noreply.github.com>
2018-05-24 12:13:43 +0200

    d

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,5 @@
-var item_template = '<div class="col-lg-4 col-md-6 mb-4"> \
+var item_template = '\
+<div class="col-lg-4 col-md-6 mb-4"> \
 <div class="card h-100"> \
   <a href="#"><img class="card-img-top" src="{image}" alt=""></a> \
   <div class="card-body"> \
@@ -31,13 +32,15 @@ function Get(yourUrl){
     return Httpreq.responseText;          
 }
 
-document.getElementById("items").innerHTML = item_template.format({
-    image: 'http://via.placeholder.com/350x150',
-    price: 'world',
-    text: 'test'
-    });
 
 function initializeItems(){
-    items_obj = JSON.parse(Get());
-
+    items_obj = JSON.parse(Get('https://raw.githubusercontent.com/tommaso-perondi/eshop/master/items.json'));
+    for(var item in items_obj.items){
+        console.log(items_obj.items[item].name);
+        document.getElementById("items").innerHTML += item_template.format({
+            image: items_obj.items[item].img,
+            price: items_obj.items[item].price,
+            text: items_obj.items[item].name
+            });
+    }
 }
